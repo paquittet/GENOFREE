20cf0d78c04401cc2a25dfcae73f9e399f624b88
Medo00000001 <cadnes.abimed@gmail.com>
2025-06-04 13:30:47 +0200

    lecture des données sur les fluo et mix par un fichier .txt

--- lecture_de_fichier_.txt.R
+++ lecture_de_fichier_.txt.R
@@ -1,3 +1,29 @@
+################################################################################
+#                                                                              #
+#         PRÉPARATION DES ÉCHELLES DE CALIBRATION PAR FLUO ET MIX              #
+#                                                                              #
+################################################################################
+#' *CONTEXTE GÉNÉRAL* :
+#' Ce bloc de code prépare automatiquement toutes les échelles de calibration 
+#' (vecteurs de positions en paires de bases) associées aux combinaisons uniques 
+#' de fluorochrome (`fluo`) et de mix (`mix`) présentes dans un fichier .txt 
+#' de référence. Ces échelles sont ensuite directement exploitables pour calibrer 
+#' les chromatogrammes via la fonction `my_ladder.info.attach()`.
+#'
+#' *OBJECTIFS* :
+#' - Lire le fichier `.txt` de référence des marqueurs.
+#' - Extraire pour chaque couple `fluochrome + mix` la liste des tailles attendues (`pb`).
+#' - Construire un objet `ladder_list` qui associe chaque combinaison à son vecteur d’échelle.
+#' - Permettre à l'utilisateur de sélectionner dynamiquement un `fluochrome` et un `mix`.
+#'
+#' *ENTRÉES* :
+#' - `ref_file` : chemin vers le fichier .txt contenant les colonnes `fluo`, `mix`, `pb`, etc.
+#'
+#' *SORTIES* :
+#' - `ladder_list` : une liste nommée, où chaque entrée est du type `"NED_mix1"` ou `"PET_mix2"`
+#'                   et contient le vecteur trié et unique des positions attendues (`pb`).
+#'
+
 # ref_file <- "/Users/medardabiona/Desktop/Genofree/data/REF_MARQUEUR_MARMOTTE.txt"
 ref_file <- "/data/REF_MARQUEUR_MARMOTTE.txt"
 
@@ -10,9 +36,10 @@ ladder_list <- list()
 # Identifier toutes les combinaisons fluo + mix
 combis <- unique(ref_marmottes[, c("fluo", "mix")])
 
+
 ################################################################################
 #                                                                              #
-# Ce que fait cette boucle :                                                   #
+# Ce que fait la boucle  for:                                                  #
 #                                                                              #
 # 1. Parcourt chaque combinaison fluo + mix                                    #       
 # 2. Construit une clé d’identification sous la forme "FAM_mix1", "LIZ_mix2".  #
@@ -24,7 +51,6 @@ combis <- unique(ref_marmottes[, c("fluo", "mix")])
 # 6. Stocke le vecteur de positions dans ladder_list[["key"]]                  #
 #                                                                              #
 ################################################################################
-
 
 for (i in seq_len(nrow(combis))) {
   fluo <- combis$fluo[i]
